4ccbd2c6776655b4d18cc70c7b5fdcc506461988
Emre Dipi <mail@emredipi.com>
2019-05-20 22:53:51 +0300

    initial commit

---
+++ app/Http/Controllers/InternshipController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\internship;
+use Illuminate\Http\Request;
+
+class InternshipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function show(internship $internship)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(internship $internship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, internship $internship)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\internship  $internship
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(internship $internship)
+    {
+        //
+    }
+}
